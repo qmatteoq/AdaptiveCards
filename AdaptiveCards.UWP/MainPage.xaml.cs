b7b0c131f49b4c1236bcf16587a845abd93756e2
Matteo Pagani <info@qmatteoq.com>
2018-03-11 17:58:26 +0100

    Added UWP and web project

---
+++ AdaptiveCards.UWP/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AdaptiveCards.Rendering.Uwp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AdaptiveCards.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OnRenderAdaptiveCard(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync(new Uri("https://adaptivecard.azurewebsites.net/api/AppConsultAdaptiveCards?code=AzSEpdNE/P0c9OFIBjro2vSKwGIlLdBWdc53/jmR7Y9PX2l1Ks0/nQ=="));
+            AdaptiveCardParseResult card = AdaptiveCard.FromJsonString(json);
+
+            AdaptiveCardRenderer renderer = new AdaptiveCardRenderer();
+            var renderResult = renderer.RenderAdaptiveCard(card.AdaptiveCard);
+
+            if (renderResult != null)
+            {
+                MainPanel.Children.Add(renderResult.FrameworkElement);
+            }
+        }
+    }
+}
---
+++ AdaptiveCards.Web/Pages/AdaptiveCard.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AdaptiveCards.Rendering.Html;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdaptiveCards.Web.Pages
+{
+    public class AdaptiveCardModel : PageModel
+    {
+        public HtmlString AdaptiveCard;
+
+        public async Task OnGet()
+        {
+            HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync("https://adaptivecard.azurewebsites.net/api/AppConsultAdaptiveCards?code=AzSEpdNE/P0c9OFIBjro2vSKwGIlLdBWdc53/jmR7Y9PX2l1Ks0/nQ==");
+            AdaptiveCardParseResult card = AdaptiveCards.AdaptiveCard.FromJson(json);
+            AdaptiveCardRenderer renderer = new AdaptiveCardRenderer();
+            RenderedAdaptiveCard renderedCard = renderer.RenderCard(card.Card);
+
+            // Get the output HTML 
+            AdaptiveCard = new HtmlString(renderedCard.Html.ToString());
+        }
+    }
+}
